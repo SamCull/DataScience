498c4e96db89f51ab43b217b0da1b1312ab4073f
Sam Cullen <samcull2000@gmail.com>
2023-11-10 10:59:51 +0000

    MSE & prediction charts
    Adding mean squared error(MSE) to get data scores
    Added chart to try predict amount of future accidents

--- main.py
+++ main.py
@@ -7,9 +7,19 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
+from statsmodels.tsa.arima.model import ARIMA
+
 
 # Load data from the CSV file
 data = pd.read_csv('carAccidentsData.csv')
+
+
+"""
+# Calculate the average of the "Casualty Age" column
+average_casualty_age = data["Casualty Age"].mean()
+
+print(f"Average Casualty Age: {average_casualty_age}")
+"""
 
 # Graph to display Accidents per year
 print(data.head())
@@ -18,6 +28,13 @@ plt.xlabel('Accident Year')
 plt.ylabel('Number of Accidents')
 plt.title('Accidents by Year')
 plt.show()
+
+# Calculate the average of the "Killed or seriously injured" column
+average_killed_or_injured = data["Killed or seriously injured"].mean()
+print(f"Average Killed or Seriously Injured: {average_killed_or_injured}")
+
+average_casualty_age = data["Casualty age"].mean()
+print(f"Average Casualty Age: {average_casualty_age}")
 
 # Graph to display Accidents per month
 monthly_data = data['Accident month'].value_counts().sort_index()
@@ -39,7 +56,6 @@ data['Weather condition'].value_counts().plot(kind='pie', autopct='%1.1f%%')
 plt.title('Distribution of Weather Conditions in Accidents')
 plt.show()
 
-
 # Handle categorical variables using one-hot encoding
 data = pd.get_dummies(data, columns=["Casualty sex", "Weather condition", "Accident month"])
 
@@ -51,7 +67,7 @@ data["Casualty age"].fillna(data["Casualty age"].median(), inplace=True)
 X = data.drop("Killed or seriously injured", axis=1)
 y = data["Killed or seriously injured"]
 
-# Split the data into training and testing sets
+# Split the data into training and testing sets                     20%
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Choose a machine learning model (e.g., Linear Regression)
@@ -65,4 +81,45 @@ y_pred = model.predict(X_test)
 
 # Evaluate the model
 mse = mean_squared_error(y_test, y_pred)
-print("Mean Squared Error:", mse)
+print("Mean Squared Error for Killed/Seriously injured:", mse)
+
+
+
+# Process the data
+data['Accident year'] = pd.to_datetime(data['Accident year'], format='%Y')
+data.set_index('Accident year', inplace=True)
+
+# Resample data to yearly frequency and count the number of accidents per year
+accidents_per_year = data.resample('Y').size()
+
+# Plot the number of accidents per year
+accidents_per_year.plot(style='o-')
+plt.xlabel('Year')
+plt.ylabel('Number of Accidents')
+plt.title('Number of Accidents per Year')
+plt.show()
+
+# Split the data into training and testing sets
+train_size = int(len(accidents_per_year) * 0.8)
+train, test = accidents_per_year[0:train_size], accidents_per_year[train_size:]
+
+# Fit ARIMA model
+model = ARIMA(train, order=(1, 1, 1))
+model_fit = model.fit()
+
+# Make predictions
+predictions = model_fit.forecast(steps=len(test))
+
+# Evaluate the model
+mse = mean_squared_error(test, predictions)
+print("Mean Squared Error for Number of Accidents:", mse)
+
+# Plot the predictions
+plt.plot(train.index, train, label='Training Data')
+plt.plot(test.index, test, label='Test Data')
+plt.plot(test.index, predictions, label='Predictions', linestyle='dashed')
+plt.xlabel('Year')
+plt.ylabel('Number of Accidents')
+plt.title('Accident predictor per Year')
+plt.legend()
+plt.show()
